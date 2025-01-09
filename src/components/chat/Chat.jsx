28d33d0a329ec80fb9e50fa21a78815a11522628
ZunairaAli18 <zunaira.ali2053@gmail.com>
2025-01-09 15:54:29 +0500

    appp

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 const Chat = () => {
     const [open, setOpen] = useState(false)
     const [text, setText] = useState(false)
     
+    const endRef = useRef(null)
+    
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behaviour: "smooth" });
+    },[]);
     const handleEmoji = e => {
         setText((prev) => prev + e.emoji);
         setOpen(false)
@@ -27,7 +32,39 @@ const Chat = () => {
                   <img src="./info.png" alt="" />
               </div>
           </div>
-          <div className="center"></div>
+          <div className="center">
+              <div className="message">
+                  <img src="./avatar.png" alt="" />
+                  <div className="texts">
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae vel officia, odit ratione sit dolor sint unde autem corrupti repellat enim dolore voluptatum numquam quidem? Sit adipisci similique impedit.</p>
+                      <span>One minute ago</span>
+                </div>
+              </div>
+              <div className="message own">
+                 
+                  <div className="texts">
+                      <img src="./userimage.jpeg" alt="" />
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae vel officia, odit ratione sit dolor sint unde autem corrupti repellat enim dolore voluptatum numquam quidem? Sit adipisci similique impedit.</p>
+                      <span>One minute ago</span>
+                </div>
+              </div>
+              <div className="message">
+                  <img src="./avatar.png" alt="" />
+                  <div className="texts">
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae vel officia, odit ratione sit dolor sint unde autem corrupti repellat enim dolore voluptatum numquam quidem? Sit adipisci similique impedit.</p>
+                      <span>One minute ago</span>
+                </div>
+              </div>
+              <div className="message own">
+                 
+                  <div className="texts">
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae vel officia, odit ratione sit dolor sint unde autem corrupti repellat enim dolore voluptatum numquam quidem? Sit adipisci similique impedit.</p>
+                      <span>One minute ago</span>
+                </div>
+              </div>
+<div ref={endRef}></div>
+          </div>
+
           <div className="bottom">
               <div className="icons">
                   <img src="./img.png" alt="" />
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,7 +1,51 @@
 import './details.css';
 const Details = () => {
   return (
-    <div className='details'>Details</div>
+    <div className='details'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Zain</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Privacy % help</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+       
+        <div className="photos">
+          <div className="photoItem">
+            <div className="photodetail">
+            <img src="./infoimg.jpg" alt="" />
+            <span>Photo_25.jpg</span>
+            </div>
+             <img src="./download.png" alt="" />
+          </div>  
+          </div>
+           </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
